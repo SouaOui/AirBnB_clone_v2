0f0d9cb7116bd9375f5b5a118e2a6fbc83e014b3
SouaOui <soumiaouzat@gmail.com>
2024-01-24 20:03:13 +0000

    modify route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route('/airbnb-onepage', methods=['GET'], strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
